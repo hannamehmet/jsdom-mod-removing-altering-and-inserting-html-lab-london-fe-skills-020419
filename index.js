e0f9aba8b8273083477d52dccc62ef00606ead4c
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-04 19:17:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
- document.createElement('main')
- document.body.appendChild(main)
- body.removeChild(body.querySelector('main'))
- 
+ var el = document.getElementById('main');
+el.remove()
 
 
 /* Create your new element here and assign it to newHeader */
