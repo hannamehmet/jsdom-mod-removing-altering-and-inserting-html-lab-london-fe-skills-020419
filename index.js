85f0693c452ffa574f44be205d89f6a3a8ee00ce
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-04 19:41:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@
 const newHeader = document.createElement('h1')
 
 newHeader; id="victory"
-insertText = "Hanna is the champion!"
+newHeader.innerHTML = "Hanna is the champion!"
 document.body.appendChild(newHeader)
 
