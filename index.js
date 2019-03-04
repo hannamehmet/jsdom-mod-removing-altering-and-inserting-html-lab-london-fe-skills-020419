8e451ca4c7281d89d00aa5e88aff9453a7c83327
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-04 19:11:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
  var element = document.createElement('div')
+ document.body.appendChild(main)
  body.removeChild(body.querySelector('main'))
  
 
