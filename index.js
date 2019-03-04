ad458fd17c083a52d60c596bcc2ba0bb2a09f214
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-04 19:42:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 /* Create your new element here and assign it to newHeader */
 
 const newHeader = document.createElement('h1')
-newHeader; id="victory"
+newHeader.id="victory"
 newHeader.innerHTML = "Hanna is the champion!"
 document.body.appendChild(newHeader)
 
